1f3fd93929a2e18ef099ec00acfc7886414f9782
mubeen7869 <137482331+mubeen7869@users.noreply.github.com>
2024-01-04 18:26:07 +0530

    third commit

---
+++ task-2.js
@@ -0,0 +1,9 @@
+console.log("Mubeen taj");
+let a= [1,4,6,0,8,5,0,0,0];
+//let a=[1,4,3,5,1,7,1,8,9,1,1,1];
+
+for(let i=0;i<a.length;i++){
+    if(a[i] !== 0){
+        console.log(a[i]);
+    }
+}
